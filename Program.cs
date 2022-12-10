1c774a33f110d5347a349b9201ee8a6477a84f20
mosukiton <harmohan.ss@live.co.uk>
2022-12-10 21:39:47 +0000

    added directories and AdventOfCode.Framework

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode.Framework;
+using AdventOfCode.Framework.Runner;
+
+SolutionRunner runner = new(
+        new Options()
+        {
+            InputBaseDirectory = "Input"
+        });
+runner.SolveLatest();
